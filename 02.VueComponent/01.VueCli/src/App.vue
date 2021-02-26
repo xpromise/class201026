11dbd1b24d1b3b4667033c0815acef50c70f627b
xiongjian <webjsforyou@gmail.com>
2021-02-26 11:51:30 +0800

    脚手架注释

--- 02.VueComponent/01.VueCli/src/App.vue
+++ 02.VueComponent/01.VueCli/src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- 使用组件 -->
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
+// 向外默认暴露组件配置对象
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App", // 给组件取一个名称（组件名称）：将来vue devtool就可以看到组件名字了
+  components: { // 注册局部组件
+    HelloWorld,
+  },
+};
 </script>
 
-<style lang="less">
+<style lang='less'>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- 02.VueComponent/01.VueCli/src/main.js
+++ 02.VueComponent/01.VueCli/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+})
